dc48d0f9c376f3b278afaf1248b8d4c4cf54c15f
aden2005 <114655613+aden2005@users.noreply.github.com>
2023-11-17 21:17:16 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# students = {
+#     "aden": [45, 50, 60, 79, 80, 96, 100],
+#     "erbol": [50, 69, 79, 80, 78, 98, 88],
+#     "aslan": [33, 44, 55, 66, 77, 88, 99]
+# }
+
+# # Добавление оценок для Tom
+# students["tom"] = [45, 46, 77, 87, 55, 45, 45]
+
+# # Вывод оценок для Aden
+# print("Aden's grades:", students["aden"])
+
+# # Вычисление среднего балла GPA для каждого студента
+# for student, grades in students.items():
+#     average_grade = sum(grades) / len(grades)
+#     print(f"{student}'s GPA: {average_grade}")
+
+
+
+
+
+dict = {"USA":"WASHINGTON","CHINA":"BEIJING","RUSSIA":"MOSCVA","KYRGYZSTAN":"BISHKEK"}
+
+print(dict.items)
