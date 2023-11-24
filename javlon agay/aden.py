4b742c018a2112e0bebd99b3cc66cd25d096adfa
aden2005 <114655613+aden2005@users.noreply.github.com>
2023-11-24 23:40:23 +0600

    Add files via upload

---
+++ javlon agay/aden.py
@@ -0,0 +1,85 @@
+# # Review Question
+# # 1) Condition in Puthon are evaluated and treated as true or false. If the condition is found to be true the program executes os found to be true, the program executes as it should, and if False,it dtops.
+# # 2) for i in range(10):
+# #     print(i)
+# # 3) break is stop the program and continue is skip one line 
+# # 4) n = int(input("Write your num")) 
+# # a = "Вход доступен"
+# # b = "Вход запрещен"
+# # if n < 10:
+# #     print(a)
+# # elif n > 10:
+# #     print(b)
+# # else:
+# #     print("error")
+
+
+# # if n < 18:
+# #     print(a)
+# # else:
+# #     print(b)
+# # 5) for i in range(10, 21):
+# #     print(i)
+# # 6) The try except block in Python is used to catch and handle exceptions.
+# #   7) i = 1
+# # while i > 5:
+# #     8)print(1)
+# #     break
+# #     i += 1
+# # 1) a) Interative
+# # 2) b) Or Operation
+# # 3) a) 1
+# # 4) c) continie
+# # 5) b) indicate the end of sequence
+# # 6) b) :
+# # 7) a) break
+# # 8) a) 0 1 2 3 4
+# # 9) a) True
+# # 10) b) ZeroDivisionError 
+# # 11) b) One
+# # 12) c) When no expection occurs
+# # 13) c) Only if some condition that has been specified is satisfied
+# # 14) c) accept
+# # 15) b) special function
+# # 16) c) finally
+# # 17) b) logical Error
+# # 18) b) Value Error
+# # 19) b) Assignment Error
+# # 20) b) range
+# # 21) a) abcd
+# # 22) b) Repeat a chunk of code until a condition is true.
+
+
+
+
+
+
+
+
+
+
+
+# from random import randint
+
+# b = randint(1, 100)
+# a = 0
+
+# while a < 10:
+#     n = int(input("Find my number: "))
+#     a += 1
+#     if n == b:
+#         print("You win")
+#         break
+#     else:
+#         if b > n:
+#             print("Your number is too low")
+#         elif b < n:
+#             print("Your number is too high")
+#         else:
+#             print("Что то не так")
+#     if a == 10:
+#         print("Game over")
+
+
+
+
---
+++ javlon agay/reviev.py
@@ -0,0 +1,22 @@
+# # # def myprint (**words):
+# # #     print (type (words))
+# # #     print(words)
+    
+    
+# # # myprint(greeting = "Helo", appologize = "I'm sorry")
+
+# # def myprint(a,b,*nums,**words ):
+# #     print("a:",a)
+# #     print("b:",b)
+# #     print(type(words))
+# #     print(words)
+# #     print(type(nums))
+# #     print(nums)
+    
+    
+# # myprint(7,8,5,3,2,4,greeting = "Hello",appologize = "I'm sorry")
+
+
+# def sum(a,b):
+#     print(a+b)
+# sum(5,5)
